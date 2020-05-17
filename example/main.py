4bdd63e25981c876f7213208111bb06d66a108ff
Theo Windebank <theowindebank@monzo.com>
2020-05-17 14:21:02 +0100

    increase frequency

--- example/main.py
+++ example/main.py
@@ -15,7 +15,7 @@ def run_main_loop():
     context = Context(
         bridge=DrHueBridge(),
         bedtime=datetime.time(hour=23, minute=45),
-        refresh_interval=1,
+        refresh_interval=0,
         webserver=True
     )
 
